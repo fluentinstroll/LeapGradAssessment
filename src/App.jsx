ccaeb8c8ca6b944d47164b14f63dd6aaf2ad2fbc
fluentinstroll <fluentinstroll@protonmail.com>
2021-07-27 18:09:32 -0400

    api now displays info

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
 import Header from './Header.jsx';
 
 const App = () => {
-
-  const [calls = [], setCalls] = useState({});
+  const [calls, setCalls] = useState([]);
   //useEffect to grab the api data
   React.useEffect(() => {
     fetchData();
@@ -14,22 +13,24 @@ const App = () => {
   //the actual function that calls the get api route
   const fetchData = () => {
     return fetch('https://aircall-job.herokuapp.com/activities')
-      .then((response) => {response.json(); console.log(response)})
-      .then((data) => console.log(data));
+      .then((response) => response.json())
+      .then((data) => setCalls(data));
   };
 
   return (
     <div className="container">
       <Header />
       <div className="container-view">
-        <div className="container-data">
-          <div>
-            <div>calls.from</div>
-            <div>tried a call on calls.to</div>
-          </div>
+        {calls.map((call) => (
+          <div className="container-data">
+            <div>
+              <div>{call.to}</div>
+              <div>tried a call on {call.from}</div>
+            </div>
 
-          <div className="call-time">calls.created_at</div>
-        </div>
+            <div className="call-time">{call.created_at}</div>
+          </div>
+        ))}
       </div>
     </div>
   );
